4cf7fca36a8b6469a9ac1129a50adb20424ed380
Igor Savin <iselwin@gmail.com>
2021-12-30 21:46:20 +0200

    Add enqueueing logic (#2)

--- src/main/kotlin/net/kiberion/ktor_scheduler/Scheduler.kt
+++ src/main/kotlin/net/kiberion/ktor_scheduler/Scheduler.kt
@@ -7,17 +7,32 @@ import io.ktor.util.AttributeKey
 import org.jobrunr.configuration.JobRunr
 import org.jobrunr.configuration.JobRunrConfiguration
 import org.jobrunr.jobs.lambdas.JobLambda
+import java.util.*
 
 class Scheduler(
     val configuration: SchedulerConfiguration,
     val scheduler: JobRunrConfiguration.JobRunrConfigurationResult
 ) {
 
-    fun recurringJob(
+    /**
+     * Schedule a job that will be repeated on specified time
+     * Specifying id that is already in use will override existing job definition
+     */
+    fun scheduleRecurringJob(
+        id: String,
         cron: String,
         job: JobLambda,
     ): String {
-        return scheduler.jobScheduler.scheduleRecurrently(cron, job)
+        return scheduler.jobScheduler.scheduleRecurrently(id, cron, job)
+    }
+
+    /**
+     * Add task to a queue that should be processed as soon as possible
+     */
+    fun scheduleEnqueuedTask(
+        job: JobLambda,
+    ): UUID {
+        return scheduler.jobScheduler.enqueue(job).asUUID()
     }
 
     companion object Feature : ApplicationFeature<Application, SchedulerConfiguration, Scheduler> {
